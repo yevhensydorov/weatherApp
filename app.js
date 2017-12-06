e7c8a1902144f02f2249dc3100495a5e89c79931
yevhensydorov <assid@ukr.net>
2017-12-06 12:28:36 +0000

    done with basic functional

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var app = express();
-var request = require("request");
+// var request = require("request");
+var fetch = require("node-fetch");
 
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 
@@ -12,12 +14,24 @@ app.get("", function(req, res){
 app.get("/results", function(req, res){
 	var searchedCity = req.query.search;
 	var url = "http://api.openweathermap.org/data/2.5/forecast?q=" + searchedCity + "&APPID=4fb207611d39b10d6e71572389d683a4&units=metric";
-	request(url, function(error, response, body){
-		if(!error && response.statusCode == 200) {
-			var data = JSON.parse(body);
-			res.render("results", {data: data});
-		}
-	});
+	
+	// FETCH DATA WITH REQUEST PACKAGE
+	// request(url, function(error, response, body){
+	// 	if(!error && response.statusCode == 200) {
+	// 		var data = JSON.parse(body);
+	// 		res.render("results", {data: data});
+	// 	}
+	// });
+
+	// FETCH DATA WITH NODE-FETCH PACKAGE
+	fetch(url)
+    .then(function(res) {
+        return res.json();
+    }).then(function(body) {
+        res.render("results", {data: body});
+    }).catch(function(err) {
+        res.render("error");
+    });
 });
 
 app.listen(4000, function(){
